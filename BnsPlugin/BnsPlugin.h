6c4d729ff3776d3c1e9b346082fa5edeeb5e1f78
Leon W. <31262614+leanleon93@users.noreply.github.com>
2025-10-04 17:19:18 +0200

    Keep only sdk headers

--- BnsPlugin/BnsPlugin.h
+++
@@ -1,2 +0,0 @@
-#pragma once
-void WINAPI BnsPlugin_Main();
--- BnsPlugin/versioninfo.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <windows.h>
-
-EXTERN_C int GetModuleVersionInfo(HMODULE hModule, PCWSTR pwszSubBlock, LPCVOID* ppv);
